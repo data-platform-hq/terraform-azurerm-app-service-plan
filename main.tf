e03ff1bcd6fc378a50b3f03a90e58066af2ae0d8
Alexander Kharkevich <alexander_kharkevich@epam.com>
2023-03-13 17:11:42 -0400

    feat: add site-scaling feature

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 resource "azurerm_service_plan" "this" {
 
-  name                = "sp-${var.project}-${var.env}-${var.location}"
-  location            = var.location
-  os_type             = var.os_type
-  resource_group_name = var.resource_group
-  tags                = var.tags
-  sku_name            = var.sku_name
+  name                     = "sp-${var.project}-${var.env}-${var.location}"
+  location                 = var.location
+  os_type                  = var.os_type
+  resource_group_name      = var.resource_group
+  tags                     = var.tags
+  sku_name                 = var.sku_name
+  per_site_scaling_enabled = var.per_site_scaling_enabled
+  worker_count             = var.worker_count
 
 }
